4d4857b0d794b506fd9d3d8d2b36de904b83cfa7
JeremieN6 <ngoyi.jeremie@gmail.com>
2025-09-29 19:59:45 +0200

    refactor:  Mise à jour du nom du projet + mise en conformité des accès aux pages/composant (uppercase to lowercase) - 3

--- src/pages/BlogContentPage.vue
+++ src/pages/BlogContentPage.vue
@@ -9,7 +9,7 @@
   
   <script setup>
   import LP_HEADER from '../components/landing/LP_Header.vue'
-    import BlogContent from '../components/Blog/BlogContent.vue'
+  import BlogContent from '../components/blog/BlogContent.vue'
   import Newsletter from '../components/landing/Newsletter.vue'
   import LP_Footer from '../components/landing/LP_Footer.vue'
   </script>
